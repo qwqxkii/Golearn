4695d1b6aaab7f901123c968b7f1d2451b9f60ba
iXiAo9 <github@xkii.cc>
2024-03-06 16:29:06 +0800

    new

--- 3-Type of data/4-Map/main.go
+++ 3-Type of data/4-Map/main.go
@@ -1,5 +1,27 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	Mapdemo()
+	println()
+	DeleteMap()
+}
+func Mapdemo() {
+	m1 := make(map[int]int, 3) //使用make申请内存
+	m1[1] = 2
+	m1[2] = 9
+	fmt.Println(m1, len(m1))
+}
+func DeleteMap() {
+	m1 := make(map[int]string, 3)
+	m1[1] = "张山"
+	m1[2] = "里斯"
+	m1[3] = "向往"
+	fmt.Println(m1, len(m1))
+	println()
+	delete(m1, 1)
+
+	fmt.Println(m1, len(m1))
+
 }
